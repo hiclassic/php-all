e0aff8efbf5c497de92514b60eacc1535b6140da
hiclassic <hares.ahmed.319@gmail.com>
2025-07-13 09:39:23 +0600

    step5 puload

--- file-upload/basic/step4.php
+++ file-upload/basic/step4.php
@@ -1,8 +1,8 @@
 <?php
 //cheek file size in associative array
-echo "<pre>";
-print_r($_FILES);
-echo "</pre>";
+// echo "<pre>";
+// print_r($_FILES);
+// echo "</pre>";
 
 if (isset($_FILES['img'])) {
     $file_name = $_FILES['img']['name'];
@@ -22,7 +22,6 @@ if (isset($_FILES['img'])) {
 }
 
 ?>
-
 <body style="text-align: center; background-color: #f2f2f2; font-family: Arial, sans-serif; ">
     <form action="" method="post" enctype="multipart/form-data" style="max-width: 300px;  margin: 10px auto;"> 
         <div> Upload: <input type="file" name="img"></div> 
